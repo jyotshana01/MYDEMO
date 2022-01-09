c74d2c6286f7326a722e87f38e37d4bdad270cc8
jyotshana <Jyotshana.21@st.niituniversity.in>
2022-01-09 11:37:25 +0000

    calc code added

---
+++ calc.java
@@ -0,0 +1,39 @@
+import java.util.scanner;
+
+public class calc {
+    public static void main(String args[]) {
+     int n1,n2,n3;
+     scanner input = new scanner(System.in);
+     system.out.println("Enter First Number");
+     n1 = input.nextInt();
+     system.out.println('Enter Second Number");
+     n2 = input.nextInt();
+     system.out.println('Choices Are:");
+     system.out.println("2. Substraction");
+     system.out.println("3. Multiplication");
+     system.out.println("4. Division");
+     system.out.println("Enter Your Choices Are:");
+     ch = input.nextInt();
+     switch(ch)
+     {
+          case 1:
+               n3 = n1 + n2;
+               system.out.println("Addition = "+n3);
+               break;
+          case 2:
+               n3 = n1 - n2;
+               system.out.println("Substraction = "+n3);
+               break;
+           case 3:
+               n3 = n1 * n2;
+               system.out.println("Multiplication = "+n3);
+               break;
+           case 4:
+               n3 = n1 / n2;
+               system.out.println("Division = "+n3);
+               break;
+           default:
+                system.out.println("You have Entered wrong Choice");
+       }
+     }
+}
