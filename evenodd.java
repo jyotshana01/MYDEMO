d1bdf18c46b901be5d53c5fdfff81c73a524b075
lohith <sadalohith@gmail.com>
2022-01-15 03:24:12 +0000

    new java files added

--- evenodd.java
+++ evenodd.java
@@ -2,15 +2,15 @@ import java.util.scanner    //util packets improting
 
 class evenodd               //class with name
 {
-public static void main(string[] args)   // main function
+Public static void main(String[] args)   // main function
 scanner sc=new scanner(system.in);       //objects  creation
-system.out.println("enter number");
+System.out.println("enter number");
 int num=sc.nextint();
 if (num%2==0)                           //condtion
 {
-system.out.println("it is even number")
+System.out.println("it is even number")
 }
 else{
-system.out.println("it is odd number")
+System.out.println("it is odd number")
 }
 }
